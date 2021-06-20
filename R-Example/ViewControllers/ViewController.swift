ee1898b80ae339ac58ccb71ec27c932fa13427b4
William Boles <wibosco@users.noreply.github.com>
2021-06-20 22:03:24 +0100

    Added some resources

--- R-Example/ViewControllers/ViewController.swift
+++ R-Example/ViewControllers/ViewController.swift
@@ -9,11 +9,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var labelTitle: UILabel!
+    @IBOutlet weak var labelDescription: UILabel!
+    
+    @IBOutlet weak var imageViewA: UIImageView!
+    @IBOutlet weak var imageViewB: UIImageView!
+    @IBOutlet weak var imageViewC: UIImageView!
+    @IBOutlet weak var imageViewD: UIImageView!
+    
+    // MARK: - ViewLifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        labelTitle.text = R.string.localizable.mainTitle()
+        labelDescription.text = R.string.localizable.mainDescription()
+        
+        imageViewA.image = R.image.animalVaccine()
+        imageViewB.image = R.image.bacteria()
+        imageViewC.image = R.image.dnaStructure()
+        imageViewD.image = R.image.bandAid()
     }
-
-
 }
 
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # R.swift-Example
 A Swift example on how to use [R.swift](https://github.com/mac-cain13/R.swift) to destring your project
+
+<div>Icons made by <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
